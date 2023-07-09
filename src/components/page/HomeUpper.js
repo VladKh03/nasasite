e6d89beebed385b2cb5de04c3648c5cf929dbfaf
Vlad Khrystianchuk <ladislaokh@gmail.com>
2023-07-09 19:41:49 +0300

    main

--- src/components/page/HomeUpper.js
+++ src/components/page/HomeUpper.js
@@ -4,6 +4,7 @@ import Carousel from "../carousel/Carousel";
 import "../styles/HomeUpper.css";
 
 const Upper = () => {
+  const API_KEY = 's49XB3dFWs287FSDEdZsFDkdaTYj7am6POMFNvh4'
   const [photosData, setPhotosData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isLoaded, setIsLoaded] = useState(false);
@@ -12,14 +13,14 @@ const Upper = () => {
     fetchPhotos();
     async function fetchPhotos() {
       const res = await fetch(
-        "https://api.nasa.gov/EPIC/api/natural/date/2019-05-30?api_key=s49XB3dFWs287FSDEdZsFDkdaTYj7am6POMFNvh4"
+        `https://api.nasa.gov/EPIC/api/natural/date/2019-05-30?api_key=${API_KEY}`
       );
       const data = await res.json();
       const imagesArray = data.map((item) => {
         return (
           "https://api.nasa.gov/EPIC/archive/natural/2019/05/30/png/" +
           item.image +
-          ".png?api_key=s49XB3dFWs287FSDEdZsFDkdaTYj7am6POMFNvh4"
+          `.png?api_key=${API_KEY}`
         );
       });
       setPhotosData(imagesArray);
@@ -39,6 +40,9 @@ const Upper = () => {
 
   return (
     <div className="carousel-section">
+      <div className="carousel-header">
+        <h1>Discover Earth</h1>
+      </div>
       <div className="carousel-container">
           {!isLoading && isLoaded && <Carousel images={photosData} />}
           {!isLoading && !isLoaded && <div>Loading...</div>}
